d39707ee9be8ce5e4d4d57c45659a6f5e69758c1
AsRealTech <asrealzy@gmail.com>
2025-08-15 15:46:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+// Load token from environment variable
+const token = process.env.BOT_TOKEN;
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers // Needed for member join events
+    ]
+});
+
+client.once('ready', () => {
+    console.log(` Logged in as ${client.user.tag}`);
+});
+
+client.on('guildMemberAdd', member => {
+    // Replace YOUR_USER_ID with your own Discord ID
+    const myUserId = process.env.CLIENT_ID;
+    
+    member.client.users.fetch(myUserId).then(user => {
+        user.send(` Someone joined ${member.guild.name}: **${member.user.tag}** `);
+    }).catch(console.error);
+});
+
+client.login(token);
