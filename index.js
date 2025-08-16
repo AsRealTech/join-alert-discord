8b57988518f961b5596840b911003f609fc8d56b
AsRealTech <asrealzy@gmail.com>
2025-08-16 10:56:00 +0100

    ok

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
-// Load token from environment variable
 const token = process.env.BOT_TOKEN;
+const alertUserId = process.env.CLIENT_ID; // Renamed for clarity
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers // Needed for member join events
+        GatewayIntentBits.GuildMembers
     ]
 });
 
 client.once('ready', () => {
-    console.log(` Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('guildMemberAdd', member => {
-    // Replace YOUR_USER_ID with your own Discord ID
-    const myUserId = process.env.CLIENT_ID;
-    
-    member.client.users.fetch(myUserId).then(user => {
-        user.send(` Someone joined ${member.guild.name}: **${member.user.tag}** `);
+    client.users.fetch(alertUserId).then(user => {
+        user.send(`Someone joined ${member.guild.name}: **${member.user.tag}**`)
+            .then(() => console.log(`Alert sent to ${user.tag}`))
+            .catch(err => console.error('Failed to send DM:', err));
     }).catch(console.error);
 });
 
